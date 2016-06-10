3137eff8b4723ed2036e8daab8c4f4edbc14c120
pathiggins13 <pat_higgins@hotmail.com>
2016-06-09 21:54:44 -0600

    fix styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-	
-	function loading(element) {
+	var parentContainer = document.getElementById("thumbnail-container")
 
-	}
+  //Controls "digest cycle" for the enlargment and change of images
 
-	function loaded() {
+  function tearDownImage() {
+    window.history.pushState({urlPath:'/'},"",'/')
+    parentContainer.removeChild(document.getElementById("photo-background"));
+    parentContainer.removeChild(document.getElementById("enlarged-photo"));
+  }
 
-	}
-
-	function checkState(photoCompleteMain, photoNumber, album) {
-
-		var enlargedImageBackground = document.createElement("div");
-		enlargedImageBackground.className = "enlarged-photo-background"
-		enlargedImageBackground.id = "photo-background"
-		enlargedImageBackground.onclick = function() { 
-			window.history.pushState({urlPath:'/'},"",'/')
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("photo-background"));
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("enlarged-photo"));
+	function digest(photoCompleteMain, photoNumber, album) {
+		var background = document.createElement("div");
+		
+    background.className = "enlarged-photo-background"
+		background.id = "photo-background"
+		background.onclick = function() { 
+			tearDownImage()
 		};
 
 		var enlargedImageDiv = document.createElement("div");
@@ -28,25 +27,21 @@ document.addEventListener("DOMContentLoaded", function() {
 		arrowLeft.className = "arrow-left"
 
 		arrowRight.onclick = function() { 
-			window.history.pushState({urlPath:'/'},"",'/')
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("photo-background"));
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("enlarged-photo"));
+			tearDownImage()
 			d = album[photoNumber + 1]
 			var https = "https://farm"
 			var domain = ".staticflickr.com/"
 			photoCompleteMain = https + d.farm + domain + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
-			checkState(photoCompleteMain, photoNumber + 1, album)
+			digest(photoCompleteMain, photoNumber + 1, album)
 		};
 
 		arrowLeft.onclick = function() {
-			window.history.pushState({urlPath:'/'},"",'/')
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("photo-background"));
-			document.getElementById("thumbnail-container").removeChild(document.getElementById("enlarged-photo"));
+			tearDownImage()
 			d = album[photoNumber - 1]
 			var https = "https://farm"
 			var domain = ".staticflickr.com/"
 			photoCompleteMain = https + d.farm + domain + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
-			checkState(photoCompleteMain, photoNumber - 1, album)
+			digest(photoCompleteMain, photoNumber - 1, album)
 		};
 
 		image.setAttribute("src", photoCompleteMain);
@@ -62,100 +57,98 @@ document.addEventListener("DOMContentLoaded", function() {
 			enlargedImageDiv.appendChild(arrowRight)
 		}
 
- 	// 	image.onload = function(){
-  //   		image.src = this.src;   
-		// };
-
-		//FIX ONLOAD EVENT FOR IMAGES USING THIS.
-		//document.getElementById("myFrame").onload = function() {myFunction()};
-
-
-		document.getElementById("thumbnail-container").insertBefore(enlargedImageDiv, document.getElementById("thumbnail-container").childNodes[0])
-		document.getElementById("thumbnail-container").insertBefore(enlargedImageBackground, document.getElementById("thumbnail-container").childNodes[0])
+		parentContainer.insertBefore(enlargedImageDiv, parentContainer.childNodes[0])
+		parentContainer.insertBefore(background, parentContainer.childNodes[0])
 	}
 
-	  function processPhotos(obj) {
-	  	var rowNum = 1
-	  	var rowName = "row-" + rowNum;
-	  	console.log("ARRAY", obj)
-	  	var photoArray = obj.photoset.photo
+  function processThumbnails(obj) {
+  	var rowNum = 1
+  	var rowName = "row-" + rowNum;
+  	var photoArray = obj.photoset.photo
 
-	  	function createRow() {
-	  		var row = document.createElement("div");
-	  		row.className = "thumbnail-row";
-	  		row.id = rowName
-	  		document.getElementById("thumbnail-container").appendChild(row);
-	  	}
+    //Creates a thumbnail image based on the photo index being passed in
+    //This includes the image and the surrounding div
 
-	  	function changeRowName() {
-	  		return rowName = "row-" + rowNum ++
-	  	}
+  	function createThumbnail(s) {
+  		var data = photoArray[s]
+  		var pictureDiv = document.createElement("div");
 
-	  	function createThumbnail(s) {
-	  		var d = photoArray[s]
-	  		var https = "https://farm"
-	  		var domain = ".staticflickr.com/"
-	  		var photoComplete = https + d.farm + domain + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
-	  		var photoCompleteMain = https + d.farm + domain + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
-	  		var pictureDiv = document.createElement("div");
+      function createPhotoUrl(d) {
+          var https = "https://farm"
+          var domain = ".staticflickr.com/"
+       return https + d.farm + domain + d.server + "/" + d.id + "_" + d.secret + "_z.jpg"
+      }
 
-	  		function createPictureDiv() {
-	  			pictureDiv.className = "thumbnail"
-	  			pictureDiv.id = "thumbnail-" + s
-	  			pictureDiv.onclick = function() { 
-	  				window.history.pushState({urlPath:'?' + d.id},"",'?' + d.id)
-	  				checkState(photoCompleteMain, s, photoArray)
-	  			};
-	  			document.getElementById("thumbnail-container").appendChild(pictureDiv);
+  		function placeholder() {
+        var ph = document.createElement("img")
+        ph.setAttribute("src", "http://www.owlhatworld.com/wp-content/uploads/2015/12/57.gif")
+        pictureDiv.appendChild(ph)
+        return pictureDiv
+      }
 
-	  		}
+      function removePlaceholder() {
+        return pictureDiv.removeChild(pictureDiv.childNodes[0]);
+      }
 
-	  		function createImg() {
-	  			var image = document.createElement("img");
-	  			image.setAttribute("src", photoComplete);
-	  			image.setAttribute("alt", "Flower");
-	  			document.getElementById(pictureDiv.id).appendChild(image);
-	  		}	
+      function createPictureDiv() {
+  			pictureDiv.className = "thumbnail"
+  			pictureDiv.id = "thumbnail-" + s
+  			
+        pictureDiv.onclick = function() { 
+  				digest(createPhotoUrl(data), s, photoArray)
+  			};
 
+        parentContainer.appendChild(placeholder())
+  		}
 
-	  		createPictureDiv()
-	  		createImg()
-	  	}
+  		function createImg() {
+  			var image = document.createElement("img");
+  			image.setAttribute("src", createPhotoUrl(data));
+  			image.setAttribute("alt", "");
 
-	  	function photoCounter() {
-	  		for (i = 0; i < photoArray.length; i++) {
-	  			createThumbnail(i)
-	  		}
-	  	}
+        image.onload = function(){
+          removePlaceholder()
+          pictureDiv.appendChild(image);
+        };
+  		}	
 
-	  	photoCounter()
-	  }
+  		createPictureDiv()
+  		createImg()
+  	}
 
+  	function photoCounter() {
+  		for (i = 0; i < photoArray.length; i++) {
+  			createThumbnail(i)
+  		}
+  	}
 
-	  	var photoReq = new XMLHttpRequest();
-		photoReq.open('GET', 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=8efcb999a58dc10aa56ce814d6ca951e&photoset_id=72157669478370805&user_id=141462154%40N07&format=json&nojsoncallback=1');
-		photoReq.send(null);
-
-
-	photoReq.onreadystatechange = function () {
-	  var DONE = 4; // readyState 4 means the request is done.
-	  var OK = 200; // status 200 is a successful return.
-	  var imagesObj;
-
-	  if (photoReq.readyState === DONE) {
-	  	if (photoReq.status === OK)
-	  		imagesObj = JSON.parse(photoReq.responseText);
-	  	console.log("IMAGES", imagesObj)
-	  	processPhotos(imagesObj)
-	  } else {
-	      console.log('Error: ' + photoReq.status); // An error occurred during the request.
-	  }
-
-
-
+    photoCounter()
 	}
 
+  function makeRequest() {
 
+  	var photoReq = new XMLHttpRequest();
+  	photoReq.open('GET', 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=8efcb999a58dc10aa56ce814d6ca951e&photoset_id=72157669478370805&user_id=141462154%40N07&format=json&nojsoncallback=1');
+  	photoReq.send(null);
+
+
+  	photoReq.onreadystatechange = function () {
+  	  var DONE = 4; // readyState 4 means the request is done.
+  	  var OK = 200; // status 200 is a successful return.
+  	  var imagesObj;
+
+  	  if (photoReq.readyState === DONE) {
+  	  	if (photoReq.status === OK)
+  	  		imagesObj = JSON.parse(photoReq.responseText);
+  	  	  processThumbnails(imagesObj)
+  	  } else {
+  	      console.log('Error: ' + photoReq.status); // An error occurred during the request.
+  	  }
+
+  	}
+  }
+
+  makeRequest()
 
 	  //https://farm1.staticflickr.com/2/1418878_1e92283336_m.jpg
 
